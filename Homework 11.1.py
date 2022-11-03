b71c9522f81659c58a2893e1427195ff6c65fef5
lcfcvards <sytnik.dmitry@gmail.com>
2022-11-03 11:38:39 +0200

    Homework 11.1

---
+++ Homework 11.1.py
@@ -0,0 +1,5 @@
+A = {"Igor", "Vasiliy", "Petr", "Ivan", "Dmitry", "Andrey", "Diana", "Yulia"}
+B = {"Arkadiy", "Anatoliy", "Petr", "Diana", "Igor", "Dmitry", "Vsevolod", "Anna"}
+
+print("Имена людей, которые должны за Сентябрь и Октябрь: ", A & B)
+print("Должники за Октябрь, у которых нет долга за Сентябрь: ", B - A)
